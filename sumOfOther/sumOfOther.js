8d3263c0fd28526ceb754193a727d15e12e144e5
vitalur <39513998+vitalur@users.noreply.github.com>
2018-10-28 18:03:44 +0300

    style: formating function

--- sumOfOther/sumOfOther.js
+++ sumOfOther/sumOfOther.js
@@ -1,9 +1,9 @@
 module.exports = function sumOfOther(array) {
-    if (array.length === 0) { return [];}
-    let sum = array.reduce(function(next, cur) {return next + cur;});
-    const newArray = [];
-    for (let i = 0; i < array.length; i++) {
-        newArray.push(sum - array[i]);
-    }
-    return newArray;
-}
+  if (array.length === 0) { return []; }
+  const sum = array.reduce(function calculate(next, cur) { return next + cur; });
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    newArray.push(sum - array[i]);
+  }
+  return newArray;
+};
