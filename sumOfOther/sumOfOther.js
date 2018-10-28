2be7977b6fc58fc5d01475f1c789e54d8ffff7ce
vitalur <39513998+vitalur@users.noreply.github.com>
2018-10-28 08:31:20 +0300

    feat package.json + test.js; fix sumOfOther
    feat create package.json and test.js 
    fix : when we call function with empty array we take empty array

--- sumOfOther/sumOfOther.js
+++ sumOfOther/sumOfOther.js
@@ -1,4 +1,5 @@
-function sumOfOther(array) {
+module.exports = function sumOfOther(array) {
+    if (array.length === 0) { return [];}
     let sum = array.reduce(function(next, cur) {return next + cur;});
     const newArray = [];
     for (let i = 0; i < array.length; i++) {
